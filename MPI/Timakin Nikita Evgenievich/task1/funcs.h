92adf5dd1fc7b326db79f6020943bf326e129126
owerbat <niktimak@gmail.com>
2018-10-10 19:59:02 +0300

    Task 1 bug fix

--- MPI/Timakin Nikita Evgenievich/task1/funcs.h
+++ MPI/Timakin Nikita Evgenievich/task1/funcs.h
@@ -6,7 +6,7 @@
 
 #define VECTOR_SIZE 20
 
-void randomize_vector(int *vec, int len); // заполнение массива случайными числами
-int maximum(int *vec, int len); // нахождение максимального элемента в массиве
-int *complete_vector(int *vec, int *len, int divider, int rem); // добавление фиктивных компонент к ветору
-void print_vector(int *vec, int len); // печать вектора
+void randomize_vector(int *vec, int len);
+int maximum(int *vec, int len);
+int *complete_vector(int *vec, int *len, int divider, int rem);
+void print_vector(int *vec, int len);
